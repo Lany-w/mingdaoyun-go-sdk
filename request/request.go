31809af8fe90874c1fcd9088c2d5962a57368b25
Lany <wlany@qq.com>
2024-01-09 09:36:52 +0800

    up

--- request/request.go
+++ request/request.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/Lany-w/mingdaoyun-go-sdk/params"
 )
@@ -12,7 +13,15 @@ import (
 func Do(url string, params params.MingDaoRequest) []byte {
 	body, _ := json.Marshal(params)
 	//fmt.Printf("url:%v, %+v \n", url, string(body))
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:    20,               // 最大空闲连接数
+			IdleConnTimeout: 30 * time.Second, // 空闲连接的超时时间
+		},
+	}
+
+	resp, err := client.Post(url, "application/json", bytes.NewBuffer(body))
 	if err != nil {
 		panic("POST request failed:" + err.Error())
 	}
