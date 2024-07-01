96def8eae80f494cf710c82b681dcb2951f6fd10
Lany <wlany@qq.com>
2024-07-01 10:26:03 +0800

    add request error log

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Lany-w/mingdaoyun-go-sdk
 
 go 1.18
+
+require github.com/kpango/glg v1.6.13
+
+require (
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/kpango/fastime v1.1.5 // indirect
+)
--- request/request.go
+++ request/request.go
@@ -3,9 +3,13 @@ package request
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/Lany-w/mingdaoyun-go-sdk/params"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/Lany-w/mingdaoyun-go-sdk/params"
+	"github.com/kpango/fastime"
+	"github.com/kpango/glg"
 )
 
 //var RequestClient *http.Client
@@ -20,10 +24,17 @@ func Do(url string, params params.MingDaoRequest) []byte {
 			IdleConnTimeout: 30 * time.Second, // 空闲连接的超时时间
 		},
 	} */
+	location, _ := time.LoadLocation("Asia/Shanghai")
 
+	fastime.SetLocation(location)
+	errlog := glg.FileWriter("./error_mingdaoyun.log", 0666)
+	defer errlog.Close()
+	glg.Get().SetMode(glg.BOTH).AddLevelWriter(glg.ERR, errlog)
 	RequestClient := &http.Client{}
 	resp, err := RequestClient.Post(url, "application/json", bytes.NewBuffer(body))
 	if err != nil {
+		glg.Error("MingDaoYun Request Error:" + err.Error())
+		glg.Error(string(body))
 		panic("POST request failed:" + err.Error())
 	}
 	defer resp.Body.Close()
